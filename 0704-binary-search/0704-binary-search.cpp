db5d2cb9513ad042d20a8ade153bc696ebfd0313
Kavay Sharma <kavaysharma1400@gmail.com>
2026-09-25 20:03:00 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.73%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target, int low, int high) {
+        if(low>high){
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[mid]>target){
+            return binarySearch(nums,target,low,mid-1);
+        }
+        return binarySearch(nums,target,mid+1,high);
+    }
+    int search(vector<int>& nums, int target){
+        return binarySearch(nums,target,0,nums.size() -1);
+    }
+};
